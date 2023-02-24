6b27b0aaa28832f750e9504956fafa96e0602054
nikolajnowicki <114876157+nikolajnowicki@users.noreply.github.com>
2023-02-24 18:14:22 +0100

    Added TS for creating list

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -5,3 +5,70 @@ const shopField = document.getElementById("name") as HTMLInputElement;
 const listContainer = document.getElementById("list") as HTMLElement;
 
 let list: ToBuy[] = [];
+
+const savedList = localStorage.getItem("shoppingList");
+if (savedList) {
+  list = JSON.parse(savedList);
+  renderList();
+}
+
+addBtn.addEventListener("click", () => {
+  const itemName = shopField.value.trim();
+  if (itemName !== "") {
+    list.push(new ToBuy(itemName, false));
+    saveList();
+    renderList();
+    shopField.value = "";
+  }
+});
+
+function renderList() {
+  listContainer.innerHTML = "";
+  const completedItems: ToBuy[] = [];
+  const incompleteItems: ToBuy[] = [];
+  for (const item of list) {
+    if (item.done) {
+      completedItems.push(item);
+    } else {
+      incompleteItems.push(item);
+    }
+  }
+  const sortedList = incompleteItems.concat(completedItems);
+  for (const item of sortedList) {
+    const listItem = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = item.done;
+    checkbox.addEventListener("change", () => {
+      item.done = checkbox.checked;
+      saveList();
+      renderList();
+    });
+    const label = document.createElement("label");
+    label.textContent = item.name;
+    if (item.done) {
+      listItem.classList.add("done");
+    }
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+    listItem.addEventListener("click", () => {
+      toggleItem(item);
+      saveList();
+      renderList();
+    });
+    listContainer.appendChild(listItem);
+  }
+}
+
+function toggleItem(item: ToBuy) {
+  if (item.done) {
+    const index = list.indexOf(item);
+    list.splice(index, 1);
+  } else {
+    item.done = true;
+  }
+}
+
+function saveList() {
+  localStorage.setItem("shoppingList", JSON.stringify(list));
+}
